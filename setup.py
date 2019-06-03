76ec269737a97a4369d153bd88f58070f6a54f98
LorenzoM1997 <lmambretti@ucdavis.edu>
2019-06-03 14:25:05 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='Chimera',
+        version='0.1',
+        description='Framework for fully modular neural networks',
+        license="MIT",
+        author='Lorenzo Mambretti',
+        author_email='lorenz.m97@gmail.com',
+        packages=['Chimera'],
+        install_requires=['numpy'],
+        )
